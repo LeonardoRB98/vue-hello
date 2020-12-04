71d83a664539a0e231a09d86f2eb3ed4363dffe5
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-12-04 16:02:44 +0100

    aggiunta img

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var app = new Vue(
   {
-    el: 'h1',
+    el: '#container',
     data: {
       message: 'HELLO WORLD!',
-      image: 'https://images.squarespace-cdn.com/content/v1/571fc5edd210b89083925aba/1587497063492-3M55NJG231XKWL9PLFL2/ke17ZwdGBToddI8pDm48kNY5czZLc7wKfNJ8Zyr68G97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmNFveLtSPA03JKuAUT-dZq1tjfBfWl-ina02RcLzKsmZD5KKSz1nInhcW0u49rfx-/Liam_Wong_Tokyo_Nights_Phone_Wallpapers_Cyberpunk_Blade_Runner_TOKYOO_TO_KY_OO_Japan_BookMinutes+To+Midnight.jpg'
-    }
+      img: 'https://wallpaperaccess.com/full/797185.png'
+    },
   }
 );
